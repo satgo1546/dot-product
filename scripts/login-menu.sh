a2733d08dd4cd5103d1b074bd7dcd0f8b4876034
Frog Chen <satgo1546@yeah.net>
2016-07-27 13:39:19 +0800

    Regular update

--- scripts/login-menu.sh
+++ scripts/login-menu.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 temp=$(mktemp)
-dialog --backtitle "$0: \$USER = $USER" --title "login-menu.sh" \
-	--no-ok --no-cancel --no-mouse --menu "$" 0 0 0 \
+msg="$(cat /etc/motd)
+↑MOTD↑ \$"
+dialog --backtitle "$0: \$USER = $USER  \$LANG = $LANG" \
+	--title "login-menu.sh" \
+	--no-ok --no-cancel --no-mouse --menu "$msg" 0 0 0 \
 	startxfce4 "Xfce" fbterm "FbTerm" clear "bash" 2>$temp
 [ $? -ne 0 ] && rm $temp && exit
 tag=$(cat $temp)

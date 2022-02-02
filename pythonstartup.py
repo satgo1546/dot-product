8cd167d9cbd1afef4ddacbdf7e3cec82b75584b4
Frog Chen <satgo1546@yeah.net>
2022-02-02 10:21:25 +0800

    Add pythonstartup.py

---
+++ pythonstartup.py
@@ -0,0 +1,5 @@
+import builtins, sys, os, time
+import string, types, struct, enum, collections, collections.abc, itertools
+import re, math, cmath, random, pathlib, json, base64
+from math import *
+import numpy as np

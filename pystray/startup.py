ebcc9e2a938d784798ec1aadfbb2d3ff35ff55b2
Frog Chen <satgo1546@yeah.net>
2025-01-20 10:42:37 +0800

    ~

--- pystray/startup.py
+++ pystray/startup.py
@@ -1,19 +1,23 @@
 import os
+import re
 import sys
 import time
 import threading
 import webbrowser
 import subprocess
+import unicodedata
+import urllib.parse
 from tkinter import messagebox
 from flask import Flask, request, send_from_directory
 from PIL import Image
+import requests
 import pystray
-
 
 
 os.chdir(os.path.dirname(__file__))
 
-app = Flask(__name__)
+app = Flask(__name__, subdomain_matching=True)
+app.config["SERVER_NAME"] = "localhost:1546"
 
 
 @app.route("/")
@@ -34,22 +38,64 @@ def serve_wsl(path):
 @app.route("/write", methods=["GET", "POST"])
 def write_file():
     if "file" not in request.files:
-        return "No file part", 400
+        return "file needed", 400
     file = request.files["file"]
     if file.filename == "":
-        return "No selected file", 400
+        return "filename needed", 400
     file.save(os.path.join("C:\\", file.filename))
-    return "File uploaded successfully", 200
+    return f"done writing {file.filename}"
+
+
+subdomains: dict[str, str] = {}
+
+
+@app.route("/<path:path>", subdomain="<subdomain>")
+def serve_dir(subdomain, path):
+    print(subdomain, subdomains[subdomain], path)
+    return send_from_directory(subdomains[subdomain], path)
+
+
+def slugify(x: str) -> str:
+    x = unicodedata.normalize("NFKD", x)
+    x = re.sub(r"[^A-Za-z0-9]", "-", x).strip("-").lower()
+    x = re.sub(r"-+", "-", x)
+    return x
+
+
+@app.route("/open")
+def open_subdomain():
+    path = request.args.get("path")
+    if not path:
+        return "path needed", 400
+    dirname, filename = os.path.split(path)
+    subdomain = slugify(os.path.basename(dirname))
+    while dirname != subdomains.get(subdomain) != None:
+        subdomain += "-copy"
+    subdomains[subdomain] = dirname
+    webbrowser.open(f"http://{subdomain}.localhost:1546/{urllib.parse.quote(filename)}")
+    return f"""allocated subdomain: {subdomain},
+        serving {dirname},
+        opening /{filename}"""
 
 
 def reload_script():
     tray.stop()
-    subprocess.Popen([sys.executable, *sys.argv], start_new_session=True, creationflags=subprocess.DETACHED_PROCESS)
+    subprocess.Popen(
+        [sys.executable, *sys.argv],
+        start_new_session=True,
+        creationflags=subprocess.DETACHED_PROCESS,
+    )
     os._exit(114)
     os.execv(sys.executable, ["python"] + sys.argv)
 
+
+if len(sys.argv) > 1:
+    for path in sys.argv[1:6]:
+        requests.get("http://localhost:1546/open", {"path": path}, timeout=1)
+    exit()
+
 time.sleep(0.5)
-server=threading.Thread(target=lambda: app.run(host="::1", port=1546))
+server = threading.Thread(target=lambda: app.run(host="::1", port=1546))
 server.start()
 
 tray = pystray.Icon(
@@ -57,8 +103,12 @@ tray = pystray.Icon(
     Image.open("icon.png"),
     "localhost:1546",
     pystray.Menu(
-        pystray.MenuItem("打开仪表盘(&O)", lambda: webbrowser.open('http://localhost:1546/'), default=True),
-        pystray.MenuItem("显示消息框", lambda:messagebox.showinfo("标题", f"正文")),
+        pystray.MenuItem(
+            "打开仪表盘(&O)",
+            lambda: webbrowser.open("http://localhost:1546/"),
+            default=True,
+        ),
+        pystray.MenuItem("显示消息框", lambda: messagebox.showinfo("标题", f"正文")),
         pystray.MenuItem("重新加载脚本(&R)", reload_script),
         pystray.MenuItem("退出(&X)", lambda: (tray.stop(), os._exit(114))),
     ),

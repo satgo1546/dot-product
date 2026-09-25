76e2a8b8ff98ac6695198eba690122d1896c8d12
Frog Chen <satgo1546@yeah.net>
2026-09-25 08:31:27 +0800

    ~

--- scripts/llm.sh
+++ scripts/llm.sh
@@ -12,4 +12,4 @@ request=$(jq --raw-input --slurp --arg prefix "$1" --arg suffix "$2" '{
 	temperature: 0,
 	top_p: 3e-45,
 }')
-curl -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Beaer $OPENAI_API_KEY" --data "$request" --no-progress-meter https://api.deepseek.com/beta/chat/completions | sed -E 's/^data: ?(\[DONE\])?//' | jq --join-output '.choices[0].delta.content'
+curl -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer $OPENAI_API_KEY" --data "$request" --no-progress-meter https://api.deepseek.com/beta/chat/completions | sed -E 's/^data: ?(\[DONE\])?//' | jq --join-output '.choices[0].delta.content'
